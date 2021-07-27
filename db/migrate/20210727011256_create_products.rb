484988382ff24a46a17285af61c5fb8279ad2ac6
Rodrigo Soares <adsrodsoares@gmail.com>
2021-07-26 22:27:11 -0300

    Adding 'paperclip' gem and generating avatar to Product

--- db/migrate/20210727011256_create_products.rb
+++ db/migrate/20210727011256_create_products.rb
@@ -1,4 +1,4 @@
-class CreateProducts < ActiveRecord::Migration[6.1]
+class Createroducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_011256) do
+ActiveRecord::Schema.define(version: 2021_07_27_012416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2021_07_27_011256) do
     t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.bigint "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
